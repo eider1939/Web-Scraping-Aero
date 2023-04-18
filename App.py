6d1df74b4cd8a6c544526366b40cd467f24b43be
eiderpeña <80364500+eider1939@users.noreply.github.com>
2023-04-17 20:19:43 -0500

    version-1.0

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 import Generadores.Fechas as Fechas
 import Generadores.Rutas as Rutas
 import Generadores.Urls as Urls
-import Latam as Latam
-import Wingo as Wingo
+import Page.Latam as Latam
+import Page.Wingo as Wingo
 import time
 from datetime import datetime
 import numpy as np
@@ -15,7 +15,7 @@ import pandas as pd
 if __name__ == "__main__":
     start_time=time.time()
     Fecha_hoy=datetime.now().strftime('%Y-%m-%d')
-    fechas=Fechas.Fecha(3)    #genera las fechas
+    fechas=Fechas.Fecha(60)    #genera las fechas
     rutas= Rutas.rutas()
     
     ###  Latam
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     Datos_Latam.to_csv("Informes/Scraping_Latam_{}.csv".format(Fecha_hoy),index=False)
 
     ### Wingo
-    urls_wingo=Urls.Urls_wingo(fechas,rutas)
-    Datos_Wingo=Wingo.Scraping_Wingo(urls_wingo)
-    Datos_Wingo['CaptureDate']=datetime.now().strftime('%Y-%m-%d')
-    Datos_Wingo.to_csv("Informes/Scraping_Wingo_{}.csv".format(Fecha_hoy),index=False)
+    #urls_wingo=Urls.Urls_wingo(fechas,rutas)
+    #Datos_Wingo=Wingo.Scraping_Wingo(urls_wingo)
+    #Datos_Wingo['CaptureDate']=datetime.now().strftime('%Y-%m-%d')
+    #Datos_Wingo.to_csv("Informes/Scraping_Wingo_{}.csv".format(Fecha_hoy),index=False)
     print('tiempo de ejecucion ',time.time() - start_time)
